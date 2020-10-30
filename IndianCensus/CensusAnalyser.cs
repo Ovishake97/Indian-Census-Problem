45b803a3cc874c4969442fbdd23f50ec5f6ae14b
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-10-30 18:21:59 +0530

    [Abhishek] Add . Added to the UC1 branch

---
+++ IndianCensus/CensusAnalyser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianCensus
+{
+   public  class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA, US
+        }
+        public Dictionary<string, CensusDTO> datamap;
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            datamap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return datamap;
+        }
+    }
+}
